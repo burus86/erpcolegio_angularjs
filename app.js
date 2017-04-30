1370bc5d81b034d42d8cb6f77eb05c5f9b1618e2
Juan Jesús Gómez Noya <burus_86@hotmail.com>
2017-04-30 04:31:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,15 +4,15 @@
 angular.module('myApp', [
 	'ngRoute',
 	'myApp.alumnos',
-	'myApp.profesores',
+	/*'myApp.profesores',
 	'myApp.ciclosformativos',
 	'myApp.asignaturas',
 	'myApp.login',
-	'myApp.home',
+	'myApp.home',*/
 	'myApp.participantes'
 ])
 .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
 	//$locationProvider.hashPrefix('!');
 	$routeProvider.otherwise({redirectTo: '/'}); //'/login'
 }])
-.constant('url', 'http://localhost/lumen/public/api/v1/') // FIXME: http://{ip_ies-sanclemente}/lumen/public/api/v1/
+.constant('url', 'http://10.21.1.100/lumen/public/api/v1/') // Si el servicio web estuviese instalado en local, dejar: http://localhost/lumen/public/api/v1/
