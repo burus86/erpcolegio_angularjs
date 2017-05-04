8209d8a0f9bbfd1bc0baa21376929493acd03891
FernandoSanchezVilas <a15fernandosv@iessanclemente.net>
2017-05-04 19:59:06 +0200

    a15fernandosv : Tarea 8

--- asignaturas/controllers/asignaturas.view.controller.js
+++ asignaturas/controllers/asignaturas.view.controller.js
@@ -4,5 +4,11 @@ angular.module('myApp.asignaturas')
 
     // TODO-1: Realizar Petición GET a la URL /asignaturas/{id}
         // TODO-2: En caso de éxito, guardar en $scope.detailsAsignatura el JSON devuelto
+        $http.get(url + 'asignaturas/'+$routeParams.id).
+        then(function(response) {
+            $scope.detailsAsignatura = response.data;
+        }, function(response) {
+            $scope.notice = response.status + " " + response.data.error;
+        });
 
 })
