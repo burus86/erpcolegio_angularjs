3e4fe47068af689fdaa2a7195e334092837ae184
unknown <a10guillermodp@iessanclemente.net>
2017-05-04 20:09:16 +0200

    GuillermoDigon:Tarea 7 Añadir funcioanalidad_Opcional

--- ciclosformativos/controllers/ciclosformativos.asignaturas.controller.js
+++ ciclosformativos/controllers/ciclosformativos.asignaturas.controller.js
@@ -2,14 +2,14 @@ angular.module('myApp.ciclosformativos')
 
 .controller("CiclosFormativosAsignaturasCtrl", function($scope, $http, $routeParams, url) { // CiclosFormativosAsignaturasCtrl controller
     $scope.detailsCiclo = {};
-        $http.get(url + 'ciclosformativos/'+$routeParams.id+'/asignaturas').
-         then(function(response) {
-            $scope.asignaturasCiclo = response.data;
-             if ($scope.asignaturasCiclo.length > 0)
-                 $scope.detailsAsignaturas = $scope.asignaturasCiclo[0].asignaturas;
-        }, function(response) {
-            $scope.notice = response.status + " " + response.data.error;
-        });
+    $http.get(url + 'ciclosformativos/'+$routeParams.id+'/asignaturas').
+    then(function(response) {
+        $scope.asignaturasCiclo = response.data;
+        if ($scope.asignaturasCiclo.length > 0)
+           $scope.detailsAsignaturas = $scope.asignaturasCiclo[0].asignaturas;
+   }, function(response) {
+    $scope.notice = response.status + " " + response.data.error;
+});
 })
 
 
