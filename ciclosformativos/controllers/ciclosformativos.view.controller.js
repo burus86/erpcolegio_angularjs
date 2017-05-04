a05997eabda6e6aab2d696553368530789e1b364
a15lorenaae <a15lorenaae@iessanclemente.net>
2017-05-04 19:50:08 +0200

    a15lorenaae:Añadir enlace a la ficha de un ciclo formativo y cargar sus datos

--- ciclosformativos/controllers/ciclosformativos.view.controller.js
+++ ciclosformativos/controllers/ciclosformativos.view.controller.js
@@ -4,5 +4,10 @@ angular.module('myApp.ciclosformativos')
 
     // TODO-1: Realizar Petición GET a la URL /ciclosformativos/{id}
         // TODO-2: En caso de éxito, guardar en $scope.detailsCiclo el JSON devuelto
-
+$http.get(url + 'ciclosformativos/'+$routeParams.id).
+        then(function(response) {
+            $scope.detailsCiclo = response.data;
+        }, function(response) {
+            $scope.notice = response.status + " " + response.data.error;
+        });
 })
