3c28596694342f807d02e58bce3a1a5616d5a53d
unknown <wcyberman@gmail.com>
2017-05-04 19:39:42 +0200

    aczio:tarea 2 funcionalidad de borrar profesor

--- alumnos/controllers/alumnos.delete.controller.js
+++ alumnos/controllers/alumnos.delete.controller.js
@@ -6,9 +6,9 @@ angular.module('myApp.alumnos')
 
         $http.delete(url + 'alumnos/'+$scope.deleteAlumno.id). //http.get(url + 'alumnos/'+id+'/delete').
             then(function(response) {
-                
+
                 angular.forEach($rootScope.alumnos, function(item, key) {
-                    if (item.id == $scope.deleteAlumno.id) 
+                    if (item.id == $scope.deleteAlumno.id)
                         $rootScope.alumnos.splice(key, 1);
                 });
                 console.log('El alumno con id = "'+$scope.deleteAlumno.id+'" y nombre = "'+$scope.deleteAlumno.nombre+' '+$scope.deleteAlumno.apellidos+'" ha sido borrado satisfactoriamente');
