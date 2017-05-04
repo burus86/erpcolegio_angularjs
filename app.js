d12cb62b44ef4e432924cae3ed3f4e4bc90d117b
a15josesf <a15josesf@iessanclemente.net>
2017-05-04 20:16:37 +0200

    zekarov: tarea10 añadir y editar de ciclos

--- app.js
+++ app.js
@@ -16,4 +16,5 @@ angular.module('myApp', [
 	$routeProvider.otherwise({redirectTo: '/'}); //'/login'
 }])
 //.constant('url', 'http://localhost/lumen/public/api/v1/') // FIXME: http://{ip_ies-sanclemente}/lumen/public/api/v1/
-.constant('url', 'http://10.21.1.100/lumen/public/api/v1/') // Si el servicio web estuviese instalado en local, dejar: http://localhost/lumen/public/api/v1/
+.constant ('url', 'http://10.21.1.100/lumen/public/api/v1/') // Si el servicio web estuviese instalado en local, dejar: http://localhost/lumen/public/api/v1/
+
